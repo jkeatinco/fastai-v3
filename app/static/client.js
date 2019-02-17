20d47faa8628ed5be89955d45f3539789d5094fd
Jeff Keating <jkeatinco@gmail.com>
2019-02-17 12:23:53 -0800

    grandmai and modal
    updated to grandmai and add modal popup

--- app/static/client.js
+++ app/static/client.js
@@ -24,6 +24,7 @@ function analyze() {
     xhr.onload = function(e) {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
+            $('#exampleModal').modal('show');
             el('result-label').innerHTML = `Result = ${response['result']}`;
         }
         el('analyze-button').innerHTML = 'Analyze';
