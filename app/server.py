de8a68727e3ad7592044dd29894492e09f9eb5e6
Jeff Keating <jkeatinco@gmail.com>
2019-08-17 21:36:24 -0700

    new pickle

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1NR6d3poIf7qaMI3LnmdH9-NQjbMdLd5C'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1h6XmAh8NR8L7siObi3v8FdUw09zUb6vj'
 export_file_name = 'export.pkl'
 
 classes = ['bong', 'vase']
